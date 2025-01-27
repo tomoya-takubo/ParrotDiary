a9ee3695159c123928014dd18e34daa98556fca4
Takubo Tomoya <tkb1003@gmail.com>
2025-01-28 01:05:30 +0900

    ヒーローセクションの実装

--- parrotprogress/app/page.tsx
+++ parrotprogress/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import styles from './styles/Home.module.css';
 
 export default function Home() {
   return (
-    <main>
-      <h1>ぱろっとぷろぐれす</h1>
+    <main className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.hero}>
+          <h1 className={styles.title}> ぱろっとぷろぐれす </h1>
+          <p className={styles.subtitle}>PartyParrotと一緒に楽しく継続</p>
+          <button className={styles.button}>
+            はじめる
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
