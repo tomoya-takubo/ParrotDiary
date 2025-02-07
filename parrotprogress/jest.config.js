dd6876752afa5879027a70c2356e01e6b1152c12
Takubo Tomoya <tkb1003@gmail.com>
2025-02-08 03:10:33 +0900

    認証機能のユニットテストを実装

---
+++ parrotprogress/jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1'
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: './tsconfig.json'
+    }]
+  }
+};
+
+module.exports = config;
