516d7eaa60466e288770c5f9f168bd66ce24d2cc
Takubo Tomoya <tkb1003@gmail.com>
2025-02-22 05:13:15 +0900

    ランディングページのスタイルの参照が誤っていたため修正

--- parrotprogress/app/page.tsx
+++ parrotprogress/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'; 
 import { ArrowRight, BookOpen, Gift, Timer } from 'lucide-react';
 import StartButton from '@/components/StartButton';
-import styles from './styles/Home.module.css';
+import styles from '@/styles/Home.module.css';
 import ParrotCollection from '@/components/ParrotCollection';
 import { createClient } from '@supabase/supabase-js';
 import { useEffect, useState } from 'react';
